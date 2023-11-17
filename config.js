8eaa0defbf981cdc9feb57d17ea1897be3d8cf07
Raees1234567890 <151110934+Raees1234567890@users.noreply.github.com>
2023-11-17 09:06:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923133735115', 'RAEES', true],
+  ['923133735115', 'RAEES', true], 
   [''] 
 ] //Number of owners
 
